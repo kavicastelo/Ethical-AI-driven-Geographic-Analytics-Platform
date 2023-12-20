1ae75dde7cf0e3eadad71957d43f57da43b89d87
kavi castelo <kavindu123450@gmail.com>
2023-12-20 13:35:54 +0530

    land use bulk import method

--- src/main/java/com/api/air_quality/controller/LandUseController.java
+++ src/main/java/com/api/air_quality/controller/LandUseController.java
@@ -3,10 +3,15 @@ package com.api.air_quality.controller;
 import com.api.air_quality.dto.ApiResponse;
 import com.api.air_quality.model.LandUseModel;
 import com.api.air_quality.repository.LandUseRepository;
+import com.api.air_quality.service.LandUseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +19,9 @@ import java.util.Optional;
 public class LandUseController {
     @Autowired
     LandUseRepository landUseRepository;
+
+    @Autowired
+    LandUseService landUseService;
 
     @PostMapping("/api/v1/saveLandUse")
     public ResponseEntity<ApiResponse> saveLandUse(@RequestBody LandUseModel landUseModel) {
@@ -56,5 +64,31 @@ public class LandUseController {
 
         ApiResponse response = new ApiResponse("Updated Code:"+id+" successfully");
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/api/v1/importLandUse")
+    public ResponseEntity<ApiResponse> importMetrological(@RequestPart("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResponseEntity.badRequest().body(new ApiResponse("File is empty"));
+        }
+
+        // Assuming you can convert MultipartFile to File
+        File convertedFile = convertMultiPartToFile(file);
+
+        landUseRepository.deleteAll();
+        landUseService.importDataFromCSV(convertedFile.getAbsolutePath());
+
+        ApiResponse response = new ApiResponse("Imported successfully");
+        return ResponseEntity.ok(response);
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return convertedFile;
     }
 }
