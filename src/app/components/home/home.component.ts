502925c2110c5cd09b486882c0cac596e2287789
kavi castelo <kavindu123450@gmail.com>
2024-01-02 21:28:49 +0530

    make theme change button as a shared component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ThemeService} from "../../services/theme.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private themeSubscription: Subscription;
+  isDarkMode: boolean | undefined;
 
+  constructor(private themeService: ThemeService) {
+    this.themeSubscription = this.themeService.getThemeObservable().subscribe((isDarkMode) => {
+      this.isDarkMode = isDarkMode;
+    });
+  }
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+  }
 }
