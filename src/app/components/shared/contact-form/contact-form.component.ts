aeeea4c0f137d0bc7ff6c8fed95f18c02594b152
kavi castelo <kavindu123450@gmail.com>
2024-01-04 21:34:47 +0530

    contact form

--- src/app/components/shared/contact-form/contact-form.component.ts
+++ src/app/components/shared/contact-form/contact-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Subscription} from "rxjs";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ThemeService} from "../../../services/theme.service";
 
 @Component({
   selector: 'app-contact-form',
@@ -6,5 +9,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+  private themeSubscription: Subscription;
+  isDarkMode: boolean | undefined;
 
+  contactForm = new FormGroup({
+    name: new FormControl(null,[
+      Validators.required
+    ]),
+    email: new FormControl(null,[
+      Validators.required,
+      Validators.email
+    ]),
+    message: new FormControl(null,[
+      Validators.required
+    ])
+  })
+
+  constructor(private themeService: ThemeService) {
+    this.themeSubscription = this.themeService.getThemeObservable().subscribe((isDarkMode) => {
+      this.isDarkMode = isDarkMode;
+    });
+  }
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+  }
+
+  submit() {
+
+  }
 }
