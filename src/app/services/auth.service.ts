a624e95010089696b58f888c1a84399174ae2573
kavi castelo <kavindu123450@gmail.com>
2024-01-07 01:07:24 +0530

    add auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {CookieService} from "ngx-cookie-service";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private cookieService: CookieService) { }
+
+  public createUser(token:string){
+    this.cookieService.set('user-token',token,60*60*24*5);
+  }
+
+  public logout(){
+    this.cookieService.delete('user-token');
+  }
+
+  public isExists():boolean{
+    let user = this.cookieService.get('user-token');
+    return user.length !== 0; //user.length === 0?false:true
+  }
+
+  public userEmail() {
+    return this.cookieService.get('user-token').toString();
+  }
+
+}
