d1d05729d51566c98408b034758756b287fc885f
kavi castelo <kavindu123450@gmail.com>
2024-01-06 16:21:00 +0530

    blog holder content

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Subscription} from "rxjs";
+import {ThemeService} from "../../services/theme.service";
 
 @Component({
   selector: 'app-blog',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent {
+  private themeSubscription: Subscription;
+  isDarkMode: boolean | undefined;
 
+  constructor(private themeService: ThemeService) {
+    this.themeSubscription = this.themeService.getThemeObservable().subscribe((isDarkMode) => {
+      this.isDarkMode = isDarkMode;
+    });
+  }
+
+  ngOnDestroy() {
+    this.themeSubscription.unsubscribe();
+  }
+
+  ngOnInit(): void {
+  }
 }
---
+++ src/app/shared/store/blog-data-store.ts
@@ -0,0 +1,9 @@
+import {BlogModel} from "../model/Blog.model";
+
+export var blogDataStore: BlogModel[] = [
+  {id: 1, title: "title1", description: "description1", content: "content1", image: "image1", tags: "tags1", created_at: "created_at1", updated_at: "updated_at1"},
+  {id: 2, title: "title2", description: "description2", content: "content2", image: "image2", tags: "tags2", created_at: "created_at2", updated_at: "updated_at2"},
+  {id: 3, title: "title3", description: "description3", content: "content3", image: "image3", tags: "tags3", created_at: "created_at3", updated_at: "updated_at3"},
+  {id: 4, title: "title4", description: "description4", content: "content4", image: "image4", tags: "tags4", created_at: "created_at4", updated_at: "updated_at4"},
+  {id: 5, title: "title5", description: "description5", content: "content5", image: "image5", tags: "tags5", created_at: "created_at5", updated_at: "updated_at5"}
+]
