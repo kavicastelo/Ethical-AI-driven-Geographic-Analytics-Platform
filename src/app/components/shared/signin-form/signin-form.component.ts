2c84fc8a2ffe300e1447a15140938fa655a478d1
kavi castelo <kavindu123450@gmail.com>
2024-01-07 18:53:48 +0530

    sign in form done

--- src/app/components/shared/signin-form/signin-form.component.ts
+++ src/app/components/shared/signin-form/signin-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signin-form',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class SigninFormComponent {
 
+  signInForm = new FormGroup({
+    email: new FormControl(null,[
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl(null,[
+      Validators.required
+    ])
+  })
+
+  submit() {
+
+  }
 }
