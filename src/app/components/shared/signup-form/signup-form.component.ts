1348b1013ed1f6d0b28cdef009d4d4996363a658
kavi castelo <kavindu123450@gmail.com>
2024-01-07 18:47:16 +0530

    sign up form done

--- src/app/components/shared/signup-form/signup-form.component.ts
+++ src/app/components/shared/signup-form/signup-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {countries} from "../../../shared/store/country-data-store";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup-form',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup-form.component.scss']
 })
 export class SignupFormComponent {
+  countries:any = countries;
 
+  signupForm = new FormGroup({
+    name: new FormControl(null,[
+      Validators.required
+    ]),
+    email: new FormControl(null,[
+      Validators.required,
+      Validators.email
+    ]),
+    phone: new FormControl(null,[
+      Validators.required
+    ]),
+    country: new FormControl(null,[
+      Validators.required
+    ]),
+    remarks: new FormControl(null)
+  })
+
+  submit() {
+
+  }
 }
