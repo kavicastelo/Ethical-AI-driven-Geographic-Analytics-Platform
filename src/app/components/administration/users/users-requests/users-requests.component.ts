795ad6aaa4665096f06967910b764ea280c4169f
kavi castelo <kavindu123450@gmail.com>
2024-01-12 15:22:31 +0530

    users-requests component content

--- src/app/components/administration/users/users-requests/users-requests.component.ts
+++ src/app/components/administration/users/users-requests/users-requests.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {usersDataStore} from "../../../../shared/store/users-data-store";
 
 @Component({
   selector: 'app-users-requests',
   templateUrl: './users-requests.component.html',
   styleUrls: ['./users-requests.component.scss']
 })
-export class UsersRequestsComponent {
+export class UsersRequestsComponent implements OnInit {
 
+  users:any = usersDataStore;
+
+  ngOnInit(): void {
+    this.users = this.users.filter((user:any) => user.active === false)
+  }
+
+  deleteUser(id:any) {
+
+  }
+
+  approveUser(id:any) {
+
+  }
 }
