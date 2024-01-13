5e1b44e116731d3d862f6dd27d256a9eb9c7c9c2
kavi castelo <kavindu123450@gmail.com>
2024-01-13 17:00:15 +0530

    admin faqs list component

--- src/app/components/administration/faqs/faqs-list/faqs-list.component.ts
+++ src/app/components/administration/faqs/faqs-list/faqs-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {faqDataStore} from "../../../../shared/store/faq-data-store";
 
 @Component({
   selector: 'app-faqs-list',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class FaqsListComponent {
 
+  faqs = faqDataStore
+
+  delete(id: any) {
+
+  }
 }
