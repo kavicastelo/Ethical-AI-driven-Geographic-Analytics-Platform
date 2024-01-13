d05905086741da28e778fe0631ca924e46f30fd2
kavi castelo <kavindu123450@gmail.com>
2024-01-13 18:14:32 +0530

    admin faqs edit component

--- src/app/components/administration/faqs/faqs-edit/faqs-edit.component.ts
+++ src/app/components/administration/faqs/faqs-edit/faqs-edit.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {faqDataStore} from "../../../../shared/store/faq-data-store";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-faqs-edit',
   templateUrl: './faqs-edit.component.html',
   styleUrls: ['./faqs-edit.component.scss']
 })
-export class FaqsEditComponent {
+export class FaqsEditComponent implements OnInit {
+  faqs = faqDataStore
+  selectedFAQ: any
 
+  faqForm = new FormGroup({
+    question: new FormControl(null,[
+      Validators.required
+    ]),
+    answer: new FormControl(null,[
+      Validators.required
+    ])
+  })
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.cdr.detectChanges();
+  }
+
+  loadCurrentFAQ(faq: any) {
+    this.faqs.forEach((f: any) => {
+      if(faq === f.question) {
+        this.selectedFAQ = f;
+        this.faqForm.get('answer')?.setValue(f.answer);
+      }
+    })
+  }
+
+  updateFAQ() {
+    console.log("updateFAQ" + this.selectedFAQ.id);
+  }
 }
