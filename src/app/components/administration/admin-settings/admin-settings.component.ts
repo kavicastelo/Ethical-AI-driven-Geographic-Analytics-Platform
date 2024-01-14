0ded76137983c7eb5ecb02a297e4821420669165
kavi castelo <kavindu123450@gmail.com>
2024-01-14 13:46:15 +0530

    admin setting init base

--- src/app/components/administration/admin-settings/admin-settings.component.ts
+++ src/app/components/administration/admin-settings/admin-settings.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {ThemeService} from "../../../services/theme.service";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-admin-settings',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-settings.component.scss']
 })
 export class AdminSettingsComponent {
+  constructor(public themeService: ThemeService, private router: Router) {}
 
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Logic to update active class based on the current route
+        this.updateActiveClass();
+      }
+    });
+  }
+
+  updateActiveClass() {
+    const currentRoute = this.router.url;
+
+    document.querySelectorAll('.nav-link').forEach(link => {
+      link.classList.remove('active');
+    });
+
+    const activeLink = document.querySelector(`.nav-link[href="${currentRoute}"]`);
+    if (activeLink) {
+      activeLink.classList.add('active');
+    }
+  }
+  isActive(s: string) {
+    return this.router.url === s;
+  }
 }
