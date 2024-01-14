13e7852f1e0844b452ea8bfcbeddc18b19651f65
kavi castelo <kavindu123450@gmail.com>
2024-01-14 13:21:34 +0530

    admin settings components

---
+++ src/app/components/administration/admin-settings/admin-settings-user-policy/admin-settings-user-policy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-settings-user-policy',
+  templateUrl: './admin-settings-user-policy.component.html',
+  styleUrls: ['./admin-settings-user-policy.component.scss']
+})
+export class AdminSettingsUserPolicyComponent {
+
+}
---
+++ src/app/components/administration/admin-settings/admin-settings-user-terms/admin-settings-user-terms.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-settings-user-terms',
+  templateUrl: './admin-settings-user-terms.component.html',
+  styleUrls: ['./admin-settings-user-terms.component.scss']
+})
+export class AdminSettingsUserTermsComponent {
+
+}
