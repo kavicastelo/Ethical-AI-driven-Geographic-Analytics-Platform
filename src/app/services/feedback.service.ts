2a5301bf2a810ab77b77b69ba48babc4b841b708
kavi castelo <kavindu123450@gmail.com>
2024-01-16 01:00:42 +0530

    Update feedback.service.ts

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -1,16 +1,50 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from "../../environments/environment";
+import {FeedbackModel} from "../shared/model/Feedback.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedbackService {
 
+  baseUrl=environment.apiUrl;
+
   constructor(private httpClient:HttpClient) { }
 
-  public feedbackLogin():Observable<any>{
-    return this.httpClient.get("http://localhost:3269/api/v1/feedback-login");
+  public getAllFeedback():Observable<any>{
+    return this.httpClient.get(this.baseUrl+"feedback");
+  }
+
+  public getFeedbackById(id:number):Observable<any>{
+    return this.httpClient.get(this.baseUrl+"feedback/"+id);
+  }
+
+  public deleteFeedback(id:number):Observable<any>{
+    return this.httpClient.delete(this.baseUrl+"feedback/delete/"+id);
+  }
+
+  public createFeedback(f:FeedbackModel):Observable<any>{
+    return this.httpClient.post(this.baseUrl+"feedback/save",{
+      name:f.name,
+      family_name:f.family_name,
+      email:f.email,
+      picture:f.picture,
+      feedback:f.feedback,
+      date:f.date
+    });
+  }
+
+  public updateFeedback(f:FeedbackModel):Observable<any>{
+    return this.httpClient.put(this.baseUrl+"feedback/update/"+f.id,{
+      name:f.name,
+      family_name:f.family_name,
+      email:f.email,
+      picture:f.picture,
+      feedback:f.feedback,
+      date:f.date
+    });
   }
 
 }
