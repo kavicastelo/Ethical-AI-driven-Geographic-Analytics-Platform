b589c0c551e910e0133d553888e8d2cc0182f2e8
kavi castelo <kavindu123450@gmail.com>
2024-01-18 00:58:57 +0530

    Update faqs-list.component.ts

--- src/app/components/administration/faqs/faqs-list/faqs-list.component.ts
+++ src/app/components/administration/faqs/faqs-list/faqs-list.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faqDataStore} from "../../../../shared/store/faq-data-store";
+import {FaqService} from "../../../../services/faq.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-faqs-list',
   templateUrl: './faqs-list.component.html',
   styleUrls: ['./faqs-list.component.scss']
 })
-export class FaqsListComponent {
+export class FaqsListComponent implements OnInit {
 
-  faqs = faqDataStore
+  faqs:any;
+
+  constructor(private faqService: FaqService, private matSnackBar: MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+    this.loadFaqs();
+  }
+
+  loadFaqs() {
+    this.faqService.getAllFAQs().subscribe((data: any) => {
+      if (data) {
+        this.faqs = data;
+      }
+    })
+  }
 
   delete(id: any) {
 
