9c127e9c0544c8e30e1f850f3ce586aebda2326a
kavi castelo <kavindu123450@gmail.com>
2024-01-17 17:34:32 +0530

    create load forecast method

--- src/app/components/administration/forecast/forecast-edit/forecast-edit.component.ts
+++ src/app/components/administration/forecast/forecast-edit/forecast-edit.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {DomSanitizer} from "@angular/platform-browser";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {forecastDataStore} from "../../../../shared/store/forecast-data-store";
+import {ForecastService} from "../../../../services/forecast.service";
 
 @Component({
   selector: 'app-forecast-edit',
@@ -10,9 +11,9 @@ import {forecastDataStore} from "../../../../shared/store/forecast-data-store";
 })
 export class ForecastEditComponent implements OnInit {
   draftItem: any;
-  forecast: any = forecastDataStore;
+  forecast: any;
 
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer, private forecastService: ForecastService) { }
 
   editForecastForm = new FormGroup({
     description: new FormControl(null,[
@@ -25,13 +26,21 @@ export class ForecastEditComponent implements OnInit {
   }
 
   loadCurrentForecast() {
-    let obj = this.forecast[0].description;
-    let arr = new Array({"description":obj});
+    this.forecastService.getForecast().subscribe(
+      res => {
+        if (res != null && res.length > 0){
+          this.forecast = res[0];
 
-    if (this.forecast[0].description != null) {
-      this.editForecastForm.setValue(arr[0]);
-    }
-    this.forecast[0].description = this.sanitizer.bypassSecurityTrustHtml(this.forecast[0].description);
+          let obj = this.forecast.description;
+          let arr = new Array({"description":obj});
+          if (this.forecast.description != null) {
+            this.editForecastForm.setValue(arr[0]);
+          }
+
+          this.forecast.description = this.sanitizer.bypassSecurityTrustHtml(this.forecast.description);
+        }
+      }
+    )
     // this.forecast[0].description = this.forecast[0].description.replace(/[\r\n]+/g," ")
   }
 
