e5e7e96e6bf14ca5b4d1e32328f3f98b48654688
kavi castelo <kavindu123450@gmail.com>
2024-01-17 22:34:32 +0530

    create load admins method

--- src/app/components/administration/users/users-admins/users-admins.component.ts
+++ src/app/components/administration/users/users-admins/users-admins.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {adminsDataStore} from "../../../../shared/store/admin-data-store";
+import {AdminService} from "../../../../services/admin.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-users-admins',
   templateUrl: './users-admins.component.html',
   styleUrls: ['./users-admins.component.scss']
 })
-export class UsersAdminsComponent {
-  admins: any[] = adminsDataStore;
+export class UsersAdminsComponent implements OnInit {
+  admins: any;
+
+  constructor(private adminService: AdminService, private matSnackbar: MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+    this.loadAdmins()
+  }
+
+  loadAdmins() {
+    this.adminService.getAllAdmins().subscribe(
+      (response) => {
+        this.admins = response;
+      }
+    )
+  }
+
+  openSnackbar(msg: string, action: string) {
+    this.matSnackbar.open(msg, action);
+  }
 }
