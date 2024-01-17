7a9fe162240b7800339e9f40670fd5b4e52a7c98
kavi castelo <kavindu123450@gmail.com>
2024-01-17 20:44:53 +0530

    create load all users method

--- src/app/components/administration/users/users-list/users-list.component.ts
+++ src/app/components/administration/users/users-list/users-list.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {usersDataStore} from "../../../../shared/store/users-data-store";
+import {UserService} from "../../../../services/user.service";
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss']
 })
-export class UsersListComponent {
-  users: any = usersDataStore;
+export class UsersListComponent implements OnInit {
+  users: any;
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userService.getAllUsers().subscribe((data: any) => {
+      if (data) {
+        this.users = data;
+      }
+    })
+  }
 
   deleteUser(id:any) {
 
