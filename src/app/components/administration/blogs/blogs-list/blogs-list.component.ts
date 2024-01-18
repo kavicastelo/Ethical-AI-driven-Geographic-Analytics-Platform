0a9032e98bd0632feb59ab2c511e9c13d338d836
kavi castelo <kavindu123450@gmail.com>
2024-01-18 19:37:37 +0530

    load blogs method

--- src/app/components/administration/blogs/blogs-list/blogs-list.component.ts
+++ src/app/components/administration/blogs/blogs-list/blogs-list.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {blogDataStore} from "../../../../shared/store/blog-data-store";
+import {BlogService} from "../../../../services/blog.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-blogs-list',
   templateUrl: './blogs-list.component.html',
   styleUrls: ['./blogs-list.component.scss']
 })
-export class BlogsListComponent {
-  blogs: any = blogDataStore
+export class BlogsListComponent implements OnInit {
+  blogs: any;
+
+  constructor(private blogService: BlogService, private matSnackBar: MatSnackBar) {
+  }
+
+  ngOnInit(): void {
+    this.loadBlogs()
+  }
+
+  loadBlogs() {
+    this.blogService.getAllBlogs().subscribe((data: any) => {
+      this.blogs = data;
+    })
+  }
 
   deleteBlog(id:any) {
 
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.matSnackBar.open(message, action, {duration: 3000});
   }
 }
