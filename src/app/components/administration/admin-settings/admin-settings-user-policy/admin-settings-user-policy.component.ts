6a3c7f315c116d589bd88d1da486bcaa7708e8b9
kavi castelo <kavindu123450@gmail.com>
2024-01-19 17:11:53 +0530

    save user policy

--- src/app/components/administration/admin-settings/admin-settings-user-policy/admin-settings-user-policy.component.ts
+++ src/app/components/administration/admin-settings/admin-settings-user-policy/admin-settings-user-policy.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {userPolicyDataStore} from "../../../../shared/store/user-policy-data-store";
+import {UserPolicyService} from "../../../../services/user-policy.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-admin-settings-user-policy',
@@ -8,15 +10,57 @@ import {userPolicyDataStore} from "../../../../shared/store/user-policy-data-sto
 })
 export class AdminSettingsUserPolicyComponent implements OnInit {
   markdownContent: string = '';
-  markdownData: any = userPolicyDataStore;
+  markdownData: any;
+  saveBtnDisabled: boolean = false
+
+  constructor(private userPolicyService: UserPolicyService, private matSnackBar: MatSnackBar) {
+  }
 
   ngOnInit(): void {
-    if (this.markdownData !== null) {
-      this.markdownContent = this.markdownData[0].markdownContent;
+    this.loadUserPolicy();
+  }
+
+  loadUserPolicy() {
+    this.userPolicyService.getAllUserPolicies().subscribe(
+      (data: any) => {
+        this.markdownData = data;
+        if (this.markdownData !== null && this.markdownData.length > 0) {
+          this.markdownContent = this.markdownData[0].markdownContent;
+        }
+        return this.markdownData
+      }
+    )
+  }
+
+  loadDate() {
+    const date = new Date().toString();
+    return date.split(' ').slice(1,4).join(' ');
+  }
+
+  savePolicy() {
+    if (this.markdownData.length > 0) {
+      this.markdownData[0].markdownContent = this.markdownContent;
+      this.saveBtnDisabled = true;
+      this.openSnackBar('Already have a policy in que. Try to update!', 'Close');
+    }
+    else {
+      this.userPolicyService.createUserPolicy({
+        id: 0,
+        markdownContent: this.markdownContent,
+        date: this.loadDate()
+      }).subscribe(res => {
+        this.openSnackBar('Policy saved successfully', 'Close');
+      }, error => {
+        this.openSnackBar('Error saving policy', 'Close');
+      })
     }
   }
 
-  log() {
-    console.log(this.markdownContent)
+  updatePolicy() {
+
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.matSnackBar.open(message, action, {duration: 3000});
   }
 }
