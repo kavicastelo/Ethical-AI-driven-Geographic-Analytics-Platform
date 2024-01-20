a03d284d4f4bfdb31b91ef1fa0103d9cc7b8c90d
kavi castelo <kavindu123450@gmail.com>
2024-01-20 19:27:02 +0530

    Update blog-det.component.ts

--- src/app/components/blog-det/blog-det.component.ts
+++ src/app/components/blog-det/blog-det.component.ts
@@ -6,6 +6,8 @@ import {Subscription} from "rxjs";
 import {DomSanitizer} from "@angular/platform-browser";
 import {commentDataStore} from "../../shared/store/comment-data-store";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {BlogService} from "../../services/blog.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-blog-det',
@@ -18,7 +20,7 @@ export class BlogDetComponent implements OnInit, OnDestroy {
   isReplyFormVisible: boolean = false;
 
   blogId: string | null | undefined;
-  blogData: any = blogDataStore;
+  blogData: any;
   commentData: any = commentDataStore;
   sortedComments: any = [];
 
@@ -33,36 +35,40 @@ export class BlogDetComponent implements OnInit, OnDestroy {
     ])
   })
 
-  constructor(private themeService: ThemeService, private route: ActivatedRoute, private sanitizer: DomSanitizer) {
+  constructor(private themeService: ThemeService, private route: ActivatedRoute, private sanitizer: DomSanitizer, private blogService: BlogService, private matSnackBar: MatSnackBar) {
     this.themeSubscription = this.themeService.getThemeObservable().subscribe((isDarkMode) => {
       this.isDarkMode = isDarkMode;
     });
   }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.blogId = params.get('id');
-      this.blogData.forEach((blog: any) => {
-        if (blog.id == this.blogId) {
-          this.blogData = blog;
-        }
-      })
-
-      this.commentData.forEach((comment: any) => {
-        if (comment.blogId == this.blogId) {
-          this.sortedComments.push(comment);
-        }
-      })
-    });
-
-    // this.subContentArray = this.blogData.content.subContent;
-
-    // Sanitize HTML content
-    this.blogData.content.mainContent = this.sanitizer.bypassSecurityTrustHtml(this.blogData.content.mainContent);
+    this.loadBlog();
   }
 
   ngOnDestroy() {
     this.themeSubscription.unsubscribe();
+  }
+
+  loadBlog() {
+    this.blogService.getAllBlogs().subscribe((blogs: any) => {
+      this.blogData = blogs;
+      this.route.paramMap.subscribe(params => {
+        this.blogId = params.get('id');
+        this.blogData.forEach((blog: any) => {
+          if (blog.id == this.blogId) {
+            this.blogData = blog;
+          }
+        })
+
+        this.commentData.forEach((comment: any) => {
+          if (comment.blogId == this.blogId) {
+            this.sortedComments.push(comment);
+          }
+        })
+      });
+
+      this.blogData.content.mainContent = this.sanitizer.bypassSecurityTrustHtml(this.blogData.content.mainContent);
+    });
   }
 
   submitReply() {
