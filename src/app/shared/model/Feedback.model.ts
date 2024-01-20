60b06d9d8acc9fdd0fec0188ac697c44c55357e0
kavi castelo <kavindu123450@gmail.com>
2024-01-20 20:44:17 +0530

    add feedback method

--- src/app/shared/model/Feedback.model.ts
+++ src/app/shared/model/Feedback.model.ts
@@ -1,9 +1,9 @@
 export interface FeedbackModel {
-  id: number;
-  name: string;
-  family_name: string;
-  email: string;
-  picture: string;
-  feedback: string;
-  date: Date;
+  id: any;
+  name: any;
+  family_name: any;
+  email: any;
+  picture: any;
+  feedback: any;
+  date: any;
 }
