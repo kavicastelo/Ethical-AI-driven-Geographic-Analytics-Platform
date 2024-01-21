c9c50934f9e390af66e1de58ccd24a19c73dd7c6
kavi castelo <kavindu123450@gmail.com>
2024-01-21 20:47:17 +0530

    add comment method

--- src/app/shared/model/Comment.model.ts
+++ src/app/shared/model/Comment.model.ts
@@ -1,11 +1,11 @@
 export interface CommentModel {
-  id: number;
-  blogId: number;
-  name: string;
-  email: string;
-  profile: string;
-  date: Date;
-  comment: string;
+  id: any;
+  blogId: any;
+  name: any;
+  email: any;
+  profile: any;
+  date: any;
+  comment: any;
   reply: any;
-  like: number;
+  like: any;
 }
