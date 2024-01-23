d001035955f2cf479367af2a18ee8a5885e1bc46
kavi castelo <kavindu123450@gmail.com>
2024-01-24 04:05:07 +0530

    Update AIModel.java

--- src/main/java/com/api/air_quality/model/ai_models/AIModel.java
+++ src/main/java/com/api/air_quality/model/ai_models/AIModel.java
@@ -137,7 +137,7 @@ public class AIModel {
     public void runScript(String file){
 
         try {
-            File scriptFile = new File("/app/"+file+".py");
+            File scriptFile = new File("/app/com/api/air_quality/"+file+".py");
             System.out.println("File exists: " + scriptFile.exists());
 
             String venvPath = "./venv";
