a0488e4ccdc7a377f55969d2d81eded3573b3540
kavi castelo <kavindu123450@gmail.com>
2024-01-25 21:15:39 +0530

    dist 2024.01.25

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api/v1/'
+  production: true,
+  // apiUrl: 'http://localhost:3000/api/v1/' // on localhost
+  apiUrl: 'https://air-quality-platform.onrender.com/api/v1/'
 };
