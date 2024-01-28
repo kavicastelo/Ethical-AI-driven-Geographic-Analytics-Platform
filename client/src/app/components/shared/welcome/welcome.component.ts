65a5a8044987d3edfdfae579efba5fd3cadfb93b
kavi castelo <kavindu123450@gmail.com>
2024-01-28 23:27:33 +0530

    welcome page content

--- client/src/app/components/shared/welcome/welcome.component.ts
+++ client/src/app/components/shared/welcome/welcome.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "../../../services/user.service";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
+
+  user: any;
+
+  constructor(private userService: UserService, private cookieService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser() {
+    this.userService.getUserByEmail(this.cookieService.userEmail()).subscribe(
+      res => {
+        this.user = res.name
+      }
+    )
+  }
 
 }
