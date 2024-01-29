bfa6dad36a2e6689809f6b461430015e0bbe7eba
kavi castelo <kavindu123450@gmail.com>
2024-01-29 19:33:53 +0530

    init sub component of dashboard monitor

---
+++ client/src/app/components/dashbord/monitor/ozone/ozone.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ozone',
+  templateUrl: './ozone.component.html',
+  styleUrls: ['./ozone.component.scss']
+})
+export class OzoneComponent {
+
+}
