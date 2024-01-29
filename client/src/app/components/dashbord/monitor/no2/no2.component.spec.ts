4cd97eae5436742bf2d274546d9c457fe5c7d329
kavi castelo <kavindu123450@gmail.com>
2024-01-29 19:59:08 +0530

    init sub components of dashboard monitor

---
+++ client/src/app/components/dashbord/monitor/no2/no2.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { No2Component } from './no2.component';
+
+describe('No2Component', () => {
+  let component: No2Component;
+  let fixture: ComponentFixture<No2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ No2Component ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(No2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/components/dashbord/monitor/pm25/pm25.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pm25',
+  templateUrl: './pm25.component.html',
+  styleUrls: ['./pm25.component.scss']
+})
+export class Pm25Component {
+
+}
