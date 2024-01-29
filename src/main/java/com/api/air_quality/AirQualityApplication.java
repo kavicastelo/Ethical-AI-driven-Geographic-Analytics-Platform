55559a86db7e275725cb970fea409d3c2e9d78c1
kavi castelo <kavindu123450@gmail.com>
2024-01-29 18:35:35 +0530

    add data generation method

--- src/main/java/com/api/air_quality/AirQualityApplication.java
+++ src/main/java/com/api/air_quality/AirQualityApplication.java
@@ -2,8 +2,10 @@ package com.api.air_quality;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AirQualityApplication {
 
 	public static void main(String[] args) {
