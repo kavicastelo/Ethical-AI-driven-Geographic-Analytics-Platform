5a7ca030cd6550aa75ebb76571a490a50add782c
kavi castelo <kavindu123450@gmail.com>
2024-01-29 18:36:51 +0530

    Revert "add data generation method"
    This reverts commit 55559a86db7e275725cb970fea409d3c2e9d78c1.

--- src/main/java/com/api/air_quality/AirQualityApplication.java
+++ src/main/java/com/api/air_quality/AirQualityApplication.java
@@ -2,10 +2,8 @@ package com.api.air_quality;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class AirQualityApplication {
 
 	public static void main(String[] args) {
