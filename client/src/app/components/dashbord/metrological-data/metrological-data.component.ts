d4b434b5adad234559a892bfa8755ba2dcca9d77
kavi castelo <kavindu123450@gmail.com>
2024-01-31 12:44:59 +0530

    metrological data component init

---
+++ client/src/app/components/dashbord/metrological-data/metrological-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metrological-data',
+  templateUrl: './metrological-data.component.html',
+  styleUrls: ['./metrological-data.component.scss']
+})
+export class MetrologicalDataComponent {
+
+}
