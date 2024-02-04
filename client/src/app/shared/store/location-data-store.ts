4205ba6906ec5113c0749c9c69d0743b454ed433
kavi castelo <kavindu123450@gmail.com>
2024-02-04 19:06:19 +0530

    locations
    update location tab in nav bar

--- client/src/app/shared/store/location-data-store.ts
+++ client/src/app/shared/store/location-data-store.ts
@@ -1,8 +1,8 @@
 import {LocationModel} from "../model/Location.model";
 
 export var locationDataStore: LocationModel[] = [
-  {id:"1",name:"CentralPark",code:"cp",type:"industrial"},
-  {id:"2",name:"Downtown",code:"dt",type:"commercial"},
-  {id:"3",name:"SuburbA",code:"sa",type:"park"},
-  {id:"4",name:"SuburbB",code:"sb",type:"residential"}
+  {id:"1",name:"Colombo",code:"cp",type:"industrial", url:"https://maps.app.goo.gl/MMZg4UvbAAT9Yn38A"},
+  {id:"2",name:"Galle",code:"dt",type:"commercial", url:"https://maps.app.goo.gl/zM2DGf72f6bmyyQL6"},
+  {id:"3",name:"Matara",code:"sa",type:"park", url:"https://maps.app.goo.gl/1gkh9eRjoBCwjNNG8"},
+  {id:"4",name:"Hambantota",code:"sb",type:"residential", url:"https://maps.app.goo.gl/LmHFEQgDRbaDaSJR9"}
 ]
